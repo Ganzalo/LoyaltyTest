075bba565af2e33460c0f43dc4e244d0c291fe2c
Ganzalo <Ganzalo2010@gmail.com>
2019-09-24 15:08:15 +0300

    add

--- src/main/java/ru/fedorov/workers/GenresHolder.java
+++ src/main/java/ru/fedorov/workers/GenresHolder.java
@@ -3,6 +3,7 @@ package ru.fedorov.workers;
 import ru.fedorov.connection.GenresHandler;
 import ru.fedorov.model.Genre;
 
+import java.util.Collections;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -10,9 +11,9 @@ public class GenresHolder {
 
     public static final Map<Integer, String> GENRES = genres();
 
-    public static Map<Integer, String> genres() {
-        return new GenresHandler().getGenres().stream()
-                .collect(Collectors.toMap(Genre::getId, Genre::getName));
+    private static Map<Integer, String> genres() {
+        return Collections.unmodifiableMap(new GenresHandler().getGenres().stream()
+                .collect(Collectors.toMap(Genre::getId, Genre::getName)));
     }
 
 }
